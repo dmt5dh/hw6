79ddd91bfc0a4f1d15d604a60586d7700620f12b
dmt5dh <dmt5dh@virginia.edu>
2014-04-26 16:34:47 -0400

    added hw6.py

--- hw6.py
+++ hw6.py
@@ -1,1 +1,24 @@
 __author__ = 'Dan'
+
+
+def main(argv):
+
+    fn = str(argv[1])
+    f = open(fn, 'r')
+
+    num_cases = f.readline()
+
+    case_input = f.readline().split()
+
+    grid_width = int(case_input[0])
+    grid_height = int(case_input[1])
+    wg = float(case_input[2])
+    wh = float(case_input[3])
+
+    print 'hello'
+    print num_cases
+    print case_input
+    print grid_width
+    print grid_height
+    print wg
+    print wh
