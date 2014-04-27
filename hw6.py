04525d6bacea49d88b23fab28342f6eb47dc4941
dmt5dh <dmt5dh@virginia.edu>
2014-04-26 22:26:03 -0400

    added hw6.py

--- hw6.py
+++ hw6.py
@@ -1,5 +1,82 @@
 __author__ = 'Dan'
 
+import sys
+import math
+import Queue
+
+
+def a_star(start, finish, grid):
+    open_list = Queue.PriorityQueue()
+    closed_list = []
+
+    open_list.put(start)
+
+    while not open_list.empty():
+        current_node = open_list.get()
+
+        if current_node[1] == finish[1]:
+            return current_node[1]  # correct return val?
+
+        closed_list.append(current_node)
+        neighbors_list = get_neighbors(current_node, grid)
+
+        for neighbor in neighbors_list:
+            if neighbor in closed_list:
+                continue
+            given_cost = current_node[1] + distance(current_node, neighbor)
+            neighbor_coord = neighbor[2]
+
+            if neighbor not in open_list:
+                final_cost = given_cost + distance(neighbor, finish)
+                neighbor_add = (final_cost, given_cost, neighbor_coord)
+                open_list.put(neighbor_add)
+
+    return None
+
+
+
+def get_neighbors(current, grid):
+    row_limit = len(grid)
+    col_limit = len(grid[0])
+
+    neighbors = []
+    row_coord = current[2][0]
+    col_coord = current[2][1]
+    if row_coord + 1 < row_limit and col_coord + 1 < col_limit and row_coord + 1 >= 0 and col_coord + 1 >= 0:
+        neighbors.append((0, 0, (row_coord + 1, col_coord + 1)))
+
+    if row_coord - 1 < row_limit and col_coord - 1 < col_limit and row_coord - 1 >= 0 and col_coord - 1 >= 0:
+        neighbors.append((0, 0, (row_coord - 1, col_coord - 1)))
+
+    if row_coord + 1 < row_limit and col_coord < col_limit and row_coord + 1 >= 0 and col_coord >= 0:
+        neighbors.append((0, 0, (row_coord + 1, col_coord)))
+
+    if row_coord - 1 < row_limit and col_coord < col_limit and row_coord - 1 >= 0 and col_coord >= 0:
+        neighbors.append((0, 0, (row_coord - 1, col_coord)))
+
+    if row_coord < row_limit and col_coord + 1 < col_limit and row_coord >= 0 and col_coord + 1 >= 0:
+        neighbors.append((0, 0, (row_coord, col_coord + 1)))
+
+    if row_coord < row_limit and col_coord - 1 < col_limit and row_coord >= 0 and col_coord - 1 >= 0:
+        neighbors.append((0, 0, (row_coord, col_coord - 1)))
+
+    if row_coord + 1 < row_limit and col_coord - 1 < col_limit and row_coord + 1 >= 0 and col_coord - 1 >= 0:
+        neighbors.append((0, 0, (row_coord + 1, col_coord - 1)))
+
+    if row_coord - 1 < row_limit and col_coord + 1 < col_limit and row_coord - 1 >= 0 and col_coord + 1 >= 0:
+        neighbors.append((0, 0, (row_coord - 1, col_coord + 1)))
+
+    return neighbors
+
+
+
+
+def distance(a, b):
+    x = math.pow((b[2][0] - a[2][0]), 2)
+    y = math.pow((b[2][1] - a[2][1]), 2)
+    d = math.sqrt(x + y)
+    return d
+
 
 def main(argv):
 
@@ -15,10 +92,46 @@ def main(argv):
     wg = float(case_input[2])
     wh = float(case_input[3])
 
-    print 'hello'
-    print num_cases
-    print case_input
-    print grid_width
-    print grid_height
-    print wg
-    print wh
+    grid = []  # Every index is a row containing list of columns
+
+    for h in range(0, grid_height):
+        row = list(f.readline().split('\n')[0])
+        grid.append(row)
+
+    start = (0, 0, (0, 0))
+    finish = (0, 0, (0, 0))
+
+    for i in grid:
+        for j in i:
+            if j == 'S':
+                start = (0, 0, (grid.index(i), i.index(j)))
+            if j == 'F':
+                finish = (0, 0, (grid.index(i), i.index(j)))
+
+    dist_sf = distance(start, finish)
+    start_cood = start[1]
+    start = (dist_sf, 0, start_cood)
+
+    heap = Queue.PriorityQueue()
+    heap.put((2.67, 3, (20, 3)))
+    heap.put((1, 4, (1, 0)))
+    heap.put((20, 0, (5, 9)))
+    heap.put((5.9, 10, (2, 15)))
+    heap.put((3.1, 9, (7, 3)))
+
+    # while not heap.empty():
+    #     print heap.get()
+    #
+    # print start
+    # print finish
+
+    print a_star(start, finish, grid)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
